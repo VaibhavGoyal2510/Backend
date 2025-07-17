f703b672097e51c56d216b1e1e017504b10f5fdd
Vaibhav Goyal <vaibhav25goyal@gmail.com>
2025-07-17 23:55:44 +0530

    User-Registration-complete

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,15 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req,res)=>{
+    // get user details from frontend
+    // validation - not empty
+    // check if user already exists: username, email
+    // check for images, check for avatar
+    // upload them to cloudinary, avatar
+    // create user object - create entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return res
 
     const {password,email,username,fullName} =req.body;
     console.log("email", email);
@@ -20,7 +29,7 @@ const registerUser = asyncHandler(async (req,res)=>{
     // }
 
     // const existingUser = User.findOne({email})
-    const existingUser = User.findOne({
+    const existingUser = await User.findOne({
         $or: [{username},{email}] 
     })
 
@@ -28,17 +37,27 @@ const registerUser = asyncHandler(async (req,res)=>{
         throw new ApiError(409,"User exists with the specified username or email")
 
     }
-
-    const localAvatarPath = req.files?.avatar[0]?.path;
-    const localCoverImagePath = req.files?.coverImage[0]?.path;
-    
+    // console.log("files ",req.files); // this was coming as undefined
+    const localAvatarPath = req.files?.avatar[0]?.path || null;
+    // const localCoverImagePath = req.files?.coverImage[0]?.path || null;
+    // console.log("File path ",localAvatarPath);
     if(!localAvatarPath){
         throw new ApiError(400,"Avatar is required ") //400 is bad request
     }
 
+    let localCoverImagePath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
+        localCoverImagePath = req.files?.coverImage[0]?.path 
+    }
+
+
+    // console.log(req.files);
+    // console.log("This is body ->",req.body)
+    
     const avatar = await uploadOnCloudinary(localAvatarPath); // That is why we added async at starting 
     // so that some tasks that require time can also be handled... like uploading takes time
-
+    
+    // console.log("This is avatar",avatar);
     const coverImage = await uploadOnCloudinary(localCoverImagePath);
 
     if (!avatar){
@@ -59,7 +78,11 @@ const registerUser = asyncHandler(async (req,res)=>{
         "-password -refreshToken"
     )
 
-    return res.status(201).json(ApiResponse(200,createdUser,"User Registered Successfully"))
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering the user")
+    }
+
+    return res.status(201).json( new ApiResponse(200,createdUser,"User Registered Successfully"))
 
 
     
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -3,26 +3,35 @@ import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINAR_CLOUD_NAME,
-    api_key:process.env.CLOUDINAR_API_KEY,    
-    api_secret: process.env.CLOUDINAR_API_SECRET
-})
+  cloud_name: process.env.CLOUDINAR_CLOUD_NAME,
+  api_key: process.env.CLOUDINAR_API_KEY,
+  api_secret: process.env.CLOUDINAR_API_SECRET,
+});
 
-const uploadOnCloudinary = async (localFilePath)=>{
-    try {
-        if(!localFilePath) return null;
-
-        const response = await cloudinary.uploader.upload(localFilePath,{
-            resource_type:"auto"      
-        })
-
-        console.log("File uploaded Successfully", response.url);
-        return response;
-        
-    } catch (error) {
-        fs.unlinkSync(localFilePath)
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+        console.log("Here I am the error")
         return null;
     }
-}
+    // console.log("Before trying ");
+    const response = await cloudinary.uploader.upload(localFilePath, {
+        resource_type: "auto",
+    });
+    // console.log("After trying ");
 
-export {uploadOnCloudinary}
+    console.log("File uploaded Successfully", response.url);
+    fs.unlinkSync(localFilePath)
+    return response;
+  } catch (error) {
+    // if (fs.existsSync(localFilePath)) {
+    //   fs.unlinkSync(localFilePath);
+    // } else {
+    //   console.warn("⚠️ File not found for deletion:", localFilePath);
+    // }
+    fs.unlinkSync(localFilePath)
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
